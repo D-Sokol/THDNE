b250947cf8bf86cd7a6f8163996f1909d557a881
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2021-02-21 04:17:02 +0300

    Add prototype of dataset class.

---
+++ dataset.py
@@ -0,0 +1,19 @@
+from torch.utils.data import Dataset
+
+
+class UnicodeRange(Dataset):
+    def __init__(self, begin, end):
+        assert end >= begin
+        self._begin = begin
+        self._end = end
+
+    def __len__(self):
+        return self._end - self._begin
+
+    def __getitem__(self, ix):
+        if not 0 <= ix < self._end:
+            raise IndexError
+        symbol = chr(self._begin + ix)
+        # TODO: some transformation
+        return symbol
+
