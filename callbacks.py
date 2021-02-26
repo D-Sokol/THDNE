8fe85ed92fe47655890008a16371ae390ee7f2bb
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2021-02-26 23:54:50 +0300

    Add callback for generating images

--- callbacks.py
+++ callbacks.py
@@ -1,3 +1,5 @@
+import itertools
+import matplotlib.pyplot as plt
 import pathlib
 import torch
 
@@ -21,4 +23,23 @@ def save_models(disc, gen, directory=pathlib.Path(), label=None):
 def load_models(disc, gen, directory=pathlib.Path(), label=None):
     disc.load_state_dict(torch.load(_filename(directory, label, True)))
     gen.load_state_dict(torch.load(_filename(directory, label, False)))
+
+
+def _generate_sample_name(directory, name, count, total_count, ext):
+    return str(directory / f"{name}{-count if total_count != 1 else ''}.{ext}")
+
+
+def sample_images(gen, noise_dataset, count=1,
+                  directory=pathlib.Path(), name='sample', ext='png'):
+    directory = pathlib.Path(directory)
+    assert directory.is_dir()
+
+    with torch.no_grad():
+        noise_it = itertools.islice(noise_dataset, count)
+        noise = torch.stack(tuple(noise_it))
+        image_stack = gen(noise).numpy().clip(0, 1).squeeze(1)
+
+    for i, image in enumerate(image_stack):
+        filename = _generate_sample_name(directory, name, i, count, ext)
+        plt.imsave(filename, image, cmap='gray')
 
