7bd40fa853f369bff3a590fcc7c2e3b4fa0c853e
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2021-03-09 22:53:19 +0300

    Implemented training mode with reading parameters from command line arguments

--- thdne.py
+++ thdne.py
@@ -4,11 +4,22 @@ import itertools
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
+from tqdm import trange
 
 from dataset import get_CJK, get_noise
 from models import get_discriminator, get_generator
+from parser import get_parser
+from callbacks import *
 
-def train(disc, gen, real_dataset, noise_dataset, n_iter=100, k=5, batch_size=100):
+
+def _get_range(*args, verbose=False):
+    return (trange if verbose else range)(*args)
+
+
+def train(disc, gen, real_dataset, noise_dataset,
+          n_iter=100, k=5, batch_size=100, verbose=False,
+          histogram_dir=None, histogram_freq=1,
+          images=1, sample_dir=None, sample_freq=1):
     optim_d = torch.optim.Adam(disc.parameters())
     optim_g = torch.optim.Adam(gen.parameters())
     noise_loader = DataLoader(noise_dataset, batch_size=batch_size)
@@ -17,7 +28,7 @@ def train(disc, gen, real_dataset, noise_dataset, n_iter=100, k=5, batch_size=10
     disc_target = torch.cat((torch.zeros(batch_size), torch.ones(batch_size)))
     gen_target = torch.ones(batch_size)
 
-    for i in range(n_iter):
+    for i in _get_range(n_iter, verbose=verbose):
         for noise_batch, real_batch in itertools.islice(zip(noise_loader, data_loader), k):
             with torch.no_grad():
                 fake_batch = gen(noise_batch)
@@ -36,24 +47,36 @@ def train(disc, gen, real_dataset, noise_dataset, n_iter=100, k=5, batch_size=10
             optim_g.step()
             optim_g.zero_grad()
 
-        # Some callbacks here
+        if histogram_dir is not None and i % histogram_freq == 0:
+            render_histograms(disc, gen, real_dataset, noise_dataset, directory=histogram_dir)
+
+        if sample_dir is not None and i % sample_freq == 0:
+            sample_images(gen, noise_dataset, count=images, directory=sample_dir)
 
 
 if __name__ == '__main__':
     EMBEDDING_SIZE = 256
 
-    # stupid but fast-to-implement behavior: call train many times and sample image after each one
-    import matplotlib.pyplot as plt
-    from tqdm import trange
-    disc = get_discriminator()
-    gen = get_generator()
+    parser = get_parser()
+    config = parser.parse_args()
+    # TODO: check arguments
 
-    real_dataset = get_CJK(size=32)
-    noise_dataset = get_noise((EMBEDDING_SIZE,))
+    if config.mode == 'train':
+        disc = get_discriminator()
+        gen = get_generator()
+        real_dataset = get_CJK(size=32)               # TODO: adjust this
+        noise_dataset = get_noise((EMBEDDING_SIZE,))  # TODO (optional): read this parameter from config
+        if config.load_dir is not None:
+            load_models(disc, gen, directory=config.load_dir, label=config.load_label)
 
-    for i in trange(100):
-        with torch.no_grad():
-            sample = gen(next(iter(noise_dataset))).numpy().clip(0, 1).squeeze()
-            plt.imsave(f'generated/sample-{i:02d}.png', sample, cmap='gray')
-        train(disc, gen, real_dataset, noise_dataset, n_iter=1)
+        train(disc, gen, real_dataset, noise_dataset,
+              n_iter=config.iter, k=config.disc_steps, batch_size=config.batch, verbose=config.verbose,
+              histogram_dir=config.histogram_dir, histogram_freq=config.histogram_freq,
+              images=config.images, sample_dir=config.sample_dir, sample_freq=config.sample_freq)
+
+        if config.save_dir is not None:
+            save_models(disc, gen, directory=config.save_dir, label=config.save_label)
+
+    elif config.mode == 'sampling':
+        raise NotImplementedError
 
